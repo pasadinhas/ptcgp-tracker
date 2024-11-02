9fbe93089b8b6dee463ca42bfaf769a016d8dbd3
Miguel Pasadinhas <pasadinhas@Miguels-MacBook-Pro.local>
2024-11-02 16:31:36 +0000

    feat: add packs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,43 @@ export default function Home() {
     <div className="p-10">
       <div className="grid grid-cols-8 gap-x-5 gap-y-5">
         {Array.from({ length: 286 }, (_, i) => i + 1).map((i) => (
-          <DexItem number={i} amount={[0,1,2][Math.floor(Math.random() * 3)]} />
+          <DexItem
+            number={i}
+            amount={[0, 1, 2][Math.floor(Math.random() * 3)]}
+          />
         ))}
       </div>
     </div>
   );
 }
 
-function DexItem({ number, amount }: { number: number, amount: number }) {
+function DexItem({ number, amount }: { number: number; amount: number }) {
   return (
     <div className="grid justify-center gap-3 shadow-xl bg-slate-200 p-3 rounded-lg">
+      <div className="flex justify-center gap-3 relative">
+        {[
+          ["mewtwo"],
+          ["pikachu"],
+          ["charizard"],
+          ["mewtwo", "pikachu", "charizard"],
+        ][Math.floor(Math.random() * 4)].map((pack) => (
+          <Image
+            src={`/static/pokemon/sets/A1/packs/${pack}.png`}
+            alt="Pack"
+            width={80}
+            height={29}
+            className="w-1/4"
+          />
+        ))}
+      </div>
       <Image
-        src={`/static/pokemon/cards/A1/${number}.png`}
+        src={`/static/pokemon/sets/A1/cards/${number}.png`}
         alt="Some pokemon card..."
         width={294}
         height={410}
-        className={`shadow-lg rounded-lg ${amount == 0 && "brightness-50 grayscale-[50%]"}`}
+        className={`shadow-lg rounded-lg ${
+          amount == 0 && "brightness-50 grayscale-[50%]"
+        }`}
       ></Image>
       <div className="flex justify-center">
         <span className="rounded-l-lg bg-slate-300 px-5 py-1">-</span>
