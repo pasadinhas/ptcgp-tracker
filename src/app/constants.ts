2fc9823455b633be531b9f62cc43345762f9b41f
Miguel Pasadinhas <pasadinhas@Miguels-MacBook-Pro.local>
2024-11-03 13:01:25 +0000

    feat: add correct packs to each card

---
+++ src/app/constants.ts
@@ -0,0 +1,17 @@
+export default {
+  RARITY_REGULAR_1: "◇",
+  RARITY_REGULAR_2: "◇◇",
+  RARITY_REGULAR_3: "◇◇◇",
+  RARITY_REGULAR_4: "◇◇◇◇",
+  RARITY_STAR_1: "☆",
+  RARITY_STAR_2: "☆☆",
+  RARITY_STAR_3: "☆☆☆",
+  RARITY_CROWN: "♛",
+  RARITY_ANY: "Any",
+
+  A1_PACK_MEWTWO: "Genetic Apex (A1)  Mewtwo",
+  A1_PACK_PIKACHU: "Genetic Apex (A1)  Pikachu",
+  A1_PACK_CHARIZARD: "Genetic Apex (A1)  Charizard",
+  A1_PACK_SPECIAL: "Genetic Apex (A1)  Special",
+  A1_PACK_ANY: "Genetic Apex (A1)  Any",
+}
